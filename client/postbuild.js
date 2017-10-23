501bb16376ebc2320a48c4df3968bf19534d7223
Arnault Nouvel <arnault.nouvel@wisepoint.fr>
2017-10-23 16:50:11 +0200

    cleanup in npm package

--- client/postbuild.js
+++ client/postbuild.js
@@ -1,2 +1,8 @@
+const rimraf = require('rimraf');
 const cpx = require("cpx");
-cpx.copy("./build/**/*.*", "../dist");
+
+console.log("replacing dist directory");
+rimraf("../dist", function () {
+    cpx.copy("./build/**/*.*", "../dist");
+    console.log("dist directory up to date");
+})
